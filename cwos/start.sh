a739fb83d5172f9eaf4ad1472974c24f7792a74f
chenwumm <158730941+chenwumm@users.noreply.github.com>
2024-05-03 15:45:23 +0800

    Create start.sh

---
+++ cwos/start.sh
@@ -0,0 +1,21 @@
+echo ""
+sleep 0.7
+echo "loading..."
+sleep 0.1
+echo "loading CPU..."
+sleep 0.1
+echo "loading storage..."
+sleep 0.1
+echo "loading system..."
+sleep 0.1
+echo "loading command..."
+sleep 0.1
+echo "loading desktop..."
+sleep 0.2
+echo "loading Chinese engine..."
+sleep 0.1
+echo "您好，世界！"
+sleep 0.01
+echo "中文引擎加载完毕"
+sleep 0.2
+echo "欢迎使用cwOS!"
